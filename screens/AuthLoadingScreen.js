19b31a1cb706bc38c7907112aaa588a1fb007284
Tanner <tanner.crockett@gmail.com>
2019-05-23 18:48:12 -0600

    Firebase Setup

--- screens/AuthLoadingScreen.js
+++ screens/AuthLoadingScreen.js
@@ -17,17 +17,7 @@ export default class AuthLoadingScreen extends React.Component {
 
   componentWillMount() {
     // Initialize Firebase
-    var firebaseConfig = {
-      apiKey: "AIzaSyDCQota6hXoEt3Vdkjj5sG1clfwVRP4M9k",
-      authDomain: "this-or-no.firebaseapp.com",
-      databaseURL: "https://this-or-no.firebaseio.com",
-      projectId: "this-or-no",
-      storageBucket: "this-or-no.appspot.com",
-      messagingSenderId: "1086022157549",
-      appId: "1:1086022157549:web:17830108357220d6"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+    
   }
 
   // Fetch the token from storage then navigate to our appropriate place
