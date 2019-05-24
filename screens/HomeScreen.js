7fdb3dc532780c94f4706e5347eb141a0ebd4249
Tanner <tanner.crockett@gmail.com>
2019-05-24 17:05:14 -0600

    add name change, message features

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import {SafeAreaView, Text, Image, AsyncStorage, FlatList, TouchableOpacity, View} from 'react-native';
+import { SafeAreaView, Text, Image, FlatList, TouchableOpacity, View } from 'react-native';
 import User from '../User';
 import firebase from 'firebase';
-import styles from '../constants/styles';
 
 export default class HomeScreen extends React.Component {
-    static navigationOptions = (navigation) => {
-        return{
+    static navigationOptions = ({ navigation }) => {
+        return {
             title: 'Chats',
             headerRight: (
-                <TouchableOpacity onPress={() => navigation.navigate('Profile', item)} >
-                    <Image source={require('../images/user.png')} style={{width:32, height:32, marginRight: 7}}/>
+                <TouchableOpacity onPress={() => navigation.navigate('Profile')} >
+                    <Image source={require('../images/user.png')} style={{ width: 32, height: 32, marginRight: 7 }} />
                 </TouchableOpacity>
             )
         }
@@ -25,43 +24,42 @@ export default class HomeScreen extends React.Component {
         dbRef.on('child_added', (val) => {
             let person = val.val();
             person.phone = val.key;
-            this.setState((prevState) => {
-                return {
-                    users: [...prevState.users, person]
-                }
-            })
+            //removed code goes here
+            if (person.phone === User.phone) {
+                User.name = person.name
+            } else {
+                this.setState((prevState) => {
+                    return {
+                        users: [...prevState.users, person]
+                    }
+                })
+            }
         })
     }
 
-    _logOut = async () => {
-        await AsyncStorage.clear();
-        this.props.navigation.navigate('Auth');
-    }
-
-    renderRow = ({item}) => {
-        return(
-            <TouchableOpacity 
+    renderRow = ({ item }) => {
+        return (
+            <TouchableOpacity
                 onPress={() => this.props.navigation.navigate('Chat', item)}
                 style={{ padding: 10, borderBottomColor: '#ccc', borderBottomWidth: 1 }}>
                 <Text style={{ fontSize: 20 }}>{item.name}</Text>
             </TouchableOpacity>
         )
     }
-  render() {
-    return (
-      <SafeAreaView>
-           <FlatList
-            data={this.state.users}
-            renderItem={this.renderRow}
-            keyExtractor={(item) => item.phone}
-           />
-      </SafeAreaView>
-    );
-  }
+    render() {
+        return (
+            <SafeAreaView>
+                <FlatList
+                    data={this.state.users}
+                    renderItem={this.renderRow}
+                    keyExtractor={(item) => item.phone}
+                />
+            </SafeAreaView>
+        );
+    }
 }
 
 // Removed Code this is needed for multiple users.
-// Line 21
 // if(person.phone === User.phone) {
 //     User.name = person.name
 // } else {
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,14 +1,57 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { SafeAreaView, Text, TextInput, AsyncStorage, TouchableOpacity, Alert } from 'react-native';
+import User from '../User';
+import styles from '../constants/styles';
+import firebase from 'firebase';
 
 export default class ProfileScreen extends React.Component {
-    render(){
-        return(
-            <View>
-                <Text>
-                    Profile Page
+    static navigationOptions = {
+        title: 'Profile'
+    }
+
+    state = {
+        name: User.name
+    }
+
+    handleChange = key => val => {
+        this.setState({
+            [key]: val
+        })
+    }
+
+    _logOut = async () => {
+        await AsyncStorage.clear();
+        this.props.navigation.navigate('Auth');
+    }
+
+    changeName = async () => {
+        if (this.state.name.length < 3) {
+            Alert.alert('Error', 'Please enter valid name');
+        } else if(User.name !== this.state.name) {
+            firebase.database().ref('users').child(User.phone).set({ name: this.state.name });
+            User.name = this.state.name;
+            Alert.alert('Success', 'Name changed successful.');
+        }
+    }
+
+    render() {
+        return (
+            <SafeAreaView style={styles.container}>
+                <Text style={{ fontSize: 20 }}>
+                    {User.phone}
                 </Text>
-            </View>
+                <TextInput
+                    style={styles.input}
+                    value={this.state.name}
+                    onChangeText={this.handleChange('name')}
+                />
+                <TouchableOpacity onPress={this.changeName}>
+                    <Text style={styles.btnText}>Change Name</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={this._logOut}>
+                    <Text style={styles.btnText}>Logout</Text>
+                </TouchableOpacity>
+            </SafeAreaView>
         )
     }
 }
